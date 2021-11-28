0c33aca79cd8c474c0bc4aa0a85bb46bf62b91d7
municorn <municorn@musicaloft.com>
2021-11-27 23:16:47 -0600

    Add `overlays` output

--- flake.nix
+++ flake.nix
@@ -10,41 +10,52 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, flake-compat }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    let
+      allSystems = flake-utils.lib.eachDefaultSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
 
-        plainPackage = pkgs.iosevka.override {
-          privateBuildPlan = builtins.readFile ./iosevka-muse.toml;
-          set = "muse";
-        };
+            plainPackage = pkgs.iosevka.override {
+              privateBuildPlan = builtins.readFile ./iosevka-muse.toml;
+              set = "muse";
+            };
 
-        nerdFontPackage = let outDir = "$out/share/fonts/truetype/"; in
-          pkgs.stdenv.mkDerivation {
-            pname = "iosevka-muse-nerd-font";
-            version = plainPackage.version;
+            nerdFontPackage = let outDir = "$out/share/fonts/truetype/"; in
+              pkgs.stdenv.mkDerivation {
+                pname = "iosevka-muse-nerd-font";
+                version = plainPackage.version;
 
-            src = ./.;
+                src = ./.;
 
-            buildInputs = [ pkgs.nerd-font-patcher ];
+                buildInputs = [ pkgs.nerd-font-patcher ];
 
-            configurePhase = "mkdir -p ${outDir}";
-            buildPhase = ''
-              for fontfile in ${plainPackage}/share/fonts/truetype/*
-              do
-                nerd-font-patcher $fontfile --complete --careful --outputdir ${outDir}
-              done
-            '';
-            dontInstall = true;
-          };
-      in
-      {
-        packages = {
-          normal = plainPackage;
-          nerd-font = nerdFontPackage;
-        };
+                configurePhase = "mkdir -p ${outDir}";
+                buildPhase = ''
+                  for fontfile in ${plainPackage}/share/fonts/truetype/*
+                  do
+                  nerd-font-patcher $fontfile --complete --careful --outputdir ${outDir}
+                  done
+                '';
+                dontInstall = true;
+              };
 
-        defaultPackage = plainPackage;
-      }
-    );
+            packages = {
+              normal = plainPackage;
+              nerd-font = nerdFontPackage;
+            };
+          in
+          {
+            inherit packages;
+            defaultPackage = plainPackage;
+          }
+        );
+    in
+    {
+      packages = allSystems.packages;
+      defaultPackage = allSystems.defaultPackage;
+      overlay = final: prev: {
+        iosevka-muse = allSystems.packages.${final.system}; # either `normal` or `nerd-font`
+      };
+    };
 }
