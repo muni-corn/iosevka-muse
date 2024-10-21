e61b8c3a337a45e1fac1e2a5f4409027078953d2
municorn <municorn@musicaloft.com>
2024-10-21 01:34:46 -0600

    feat: update flake and variants

--- flake.nix
+++ flake.nix
@@ -2,61 +2,59 @@
   description = "Iosevka - custom Muse variant";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat }:
-    let
-      allSystems = flake-utils.lib.eachDefaultSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    (flake-utils.lib.eachDefaultSystem
+      (
+        system: let
+          pkgs = import nixpkgs {inherit system;};
 
-            plainPackage = pkgs.iosevka.override {
-              privateBuildPlan = builtins.readFile ./iosevka-muse.toml;
-              set = "muse";
-            };
+          plainPackage = pkgs.iosevka.override {
+            privateBuildPlan = builtins.readFile ./iosevka-muse.toml;
+            set = "Muse";
+          };
 
-            nerdFontPackage = let outDir = "$out/share/fonts/truetype/"; in
-              pkgs.stdenv.mkDerivation {
-                pname = "iosevka-muse-nerd-font";
-                version = plainPackage.version;
+          nerdFontPackage = let
+            outDir = "$out/share/fonts/truetype/";
+          in
+            pkgs.stdenv.mkDerivation {
+              pname = "iosevka-muse-nerd-font";
+              version = plainPackage.version;
 
-                src = builtins.path { path = ./.; name = "iosevka-muse"; };
-
-                buildInputs = [ pkgs.nerd-font-patcher ];
-
-                configurePhase = "mkdir -p ${outDir}";
-                buildPhase = ''
-                  for fontfile in ${plainPackage}/share/fonts/truetype/*
-                  do
-                  nerd-font-patcher $fontfile --complete --careful --outputdir ${outDir}
-                  done
-                '';
-                dontInstall = true;
+              src = builtins.path {
+                path = ./.;
+                name = "iosevka-muse";
               };
 
-            packages = {
-              normal = plainPackage;
-              nerd-font = nerdFontPackage;
+              buildInputs = [pkgs.nerd-font-patcher];
+
+              configurePhase = "mkdir -p ${outDir}";
+              buildPhase = ''
+                for fontfile in ${plainPackage}/share/fonts/truetype/*
+                do
+                nerd-font-patcher $fontfile --complete --careful --outputdir ${outDir}
+                done
+              '';
+              dontInstall = true;
             };
-          in
-          {
-            inherit packages;
-            defaultPackage = plainPackage;
-          }
-        );
-    in
-    {
-      packages = allSystems.packages;
-      defaultPackage = allSystems.defaultPackage;
+        in {
+          packages = {
+            default = plainPackage;
+            normal = plainPackage;
+            nerd-font = nerdFontPackage;
+          };
+        }
+      ))
+    // {
       overlay = final: prev: {
-        iosevka-muse = allSystems.packages.${final.system}; # either `normal` or `nerd-font`
+        iosevka-muse = self.packages.${final.system}; # either `normal` or `nerd-font`
       };
     };
 }
